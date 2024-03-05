478569241361ec8673320d47163cb3845baedfa4
Reyes <rb7885074@gmail.com>
2024-03-05 15:24:19 -0600

    prueba led

--- controllers/empleado.controller.js
+++ controllers/empleado.controller.js
@@ -1,3 +1,4 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import Empleados from "../models/empleados.modelo"
+
--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import ClienteRouter from "./routes/cliente.routes.js"
+import bodyParser from "body-parser";
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 dotenv.config();
 
 app.use(cors());
@@ -12,22 +16,45 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-mongoose.connect(process.env.MONGO_URL_DATABASE,{
+mongoose.connect(process.env.MONGO_URL_DATABASE, {
     useNewUrlParser: true,
 });
 
 const db = mongoose.connection;
-db.on('error',console.error.bind(console,"Error en la conexion a mongodb"));
-db.once("open",()=>console.log("Conexion exitosa a MongoDB"));
+db.on('error', console.error.bind(console, "Error en la conexion a mongodb"));
+db.once("open", () => console.log("Conexion exitosa a MongoDB"));
 
-app.listen(3000,()=>console.log("Servidor conectado"));
+app.listen(3000, () => console.log("Servidor conectado"));
 
 //rutas
-app.use("/api/cliente/",ClienteRouter);
-app.use("/prueba",(req,res) =>res.send("Hello"));
+app.use("/api/cliente/", ClienteRouter);
+const pinLed = 13; // Puerto digital donde está conectado tu LED
+let ledEncendido = false;
+app.get('/led/encender', (req, res) => {
+    if (!ledEncendido) {
+        // Enciende el LED solo si no está encendido
+        ledEncendido = true;
+        // Aquí deberías agregar el código para encender el LED usando Arduino o cualquier dispositivo
+        console.log('LED encendido');
+        res.status(200).send('LED encendido');
+    } else {
+        res.status(400).send('El LED ya está encendido');
+    }
+});
+app.get('/led/apagar', (req, res) => {
+    if (ledEncendido) {
+        // Apaga el LED solo si está encendido
+        ledEncendido = false;
+        // Aquí deberías agregar el código para apagar el LED usando Arduino o cualquier dispositivo
+        console.log('LED apagado');
+        res.status(200).send('LED apagado');
+    } else {
+        res.status(400).send('El LED ya está apagado');
+    }
+});
 
 //middleware
-app.use((err,req,res,next) => {
+app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal server error";
     return res.status(statusCode).json({
