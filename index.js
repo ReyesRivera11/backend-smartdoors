1103f8deede82f3f04d968ba697e4008a5923b4a
Reyes <rb7885074@gmail.com>
2024-03-04 20:27:40 -0600

    prueba

--- index.js
+++ index.js
@@ -14,20 +14,18 @@ app.use(cookieParser());
 
 mongoose.connect(process.env.MONGO_URL_DATABASE,{
     useNewUrlParser: true,
-    useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
 db.on('error',console.error.bind(console,"Error en la conexion a mongodb"));
 db.once("open",()=>console.log("Conexion exitosa a MongoDB"));
 
-app.listen(3000,()=>console.log("Servidor en http://localhost:3000"));
+app.listen(3000,()=>console.log("Servidor conectado"));
 
 //rutas
 app.use("/api/cliente/",ClienteRouter);
-app.use("/prueba",(req,res)=>{
-    res.send("Hello")
-})
+app.use("/prueba",(req,res) =>res.send("Hello"));
+
 //middleware
 app.use((err,req,res,next) => {
     const statusCode = err.statusCode || 500;
