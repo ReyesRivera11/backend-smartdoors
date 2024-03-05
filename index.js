4772d0795210bad48c2587e09d556b18e4d96e1f
Reyes <rb7885074@gmail.com>
2024-03-04 18:36:42 -0600

    cambios

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ import ClienteRouter from "./routes/cliente.routes.js"
 const app = express();
 dotenv.config();
 
-app.use(cors({
-    origin:'http://localhost:5173',
-    credentials:true
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
