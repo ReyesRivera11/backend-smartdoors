9c65bcd9694d828f5c31ad689fae3329f0c1058a
Reyes <rb7885074@gmail.com>
2024-03-09 13:49:32 -0600

    correccion

--- controllers/cliente.controller.js
+++ controllers/cliente.controller.js
@@ -44,7 +44,7 @@ export const login = async (req, res, next) => {
         res.cookie("access_token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "None",
           })
         res.json(rest);
     } catch (error) {
