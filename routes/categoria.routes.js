8092dd13916bbb7dd2884a6e9ab8625bd072c8d8
Reyes <rb7885074@gmail.com>
2024-03-11 20:44:35 -0600

    ruta corregida editar

--- routes/categoria.routes.js
+++ routes/categoria.routes.js
@@ -6,7 +6,7 @@ const routerCategoria = express.Router();
 routerCategoria.post("/agregar",agregar);
 routerCategoria.get("/listar",listar);
 routerCategoria.get("/buscar/:id",burcarPorId);
-routerCategoria.post("/editar/:id",editar);
+routerCategoria.put("/editar/:id",editar);
 routerCategoria.delete("/eliminar/:id",eliminar);
 
 export default routerCategoria; 
