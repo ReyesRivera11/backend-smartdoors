25ff430f5f19399acb3efc12a31731bca525dfd2
Reyes <rb7885074@gmail.com>
2024-03-21 11:46:10 -0600

    agregar cpoliticas corregido

--- routes/politicas.routes.js
+++ routes/politicas.routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/listar",listar);
 router.delete("/eliminar/:id",eliminar);
 router.get("/editar/:id",editar);
-router.get("/agregar",agregar);
+router.post("/agregar",agregar);
 router.get("/getById/:id",obtenerById);
 
 export default router;
