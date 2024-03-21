963699f8e27f3484ac12fa0e8f9826342db0eaaf
Reyes <rb7885074@gmail.com>
2024-03-20 23:42:30 -0600

    editar pregunta w

--- routes/pregunta.routes.js
+++ routes/pregunta.routes.js
@@ -1,11 +1,11 @@
 import express  from "express";
-import { agregar, editar, eliminar, listar } from "../controllers/pregunta.controller.js";
+import { agregar, editar, eliminar, getPregunta, listar } from "../controllers/pregunta.controller.js";
 
 const router = express.Router();
 router.get("/listar",listar);
 router.post("/agregar",agregar);
 router.delete("/eliminar/:id",eliminar);
 router.put("/editar/:id",editar);
-router.get("/getPregunta/:id",editar);
+router.get("/getPregunta/:id",getPregunta);
 
 export default router;
