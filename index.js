2008855007833a83829c1c16bd11e5b56154d1ba
Reyes <rb7885074@gmail.com>
2024-04-04 15:31:23 -0600

    cors

--- index.js
+++ index.js
@@ -28,11 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 dotenv.config();
 
-app.use(cors({
-  // origin:'https://doorcraft.developers506.com',
-  origin:'https://doorcraftt.developers506.com',
-  credentials:true
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
